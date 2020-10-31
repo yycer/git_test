cc252b6073f8f6c45fd53acd29d4fa7c0b49372b
yycer <15900617310@163.com>
2020-10-31 10:16:57 +0800

    Initial commit

---
+++ src/main/java/com/frankie/git_test/Test.java
@@ -0,0 +1,12 @@
+package com.frankie.git_test;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/10/31 10:15
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
