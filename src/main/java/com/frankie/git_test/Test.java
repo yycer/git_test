924ca7dbc4cb259f3a5789ff70517789b0677aa8
yycer <15900617310@163.com>
2020-11-03 22:19:09 +0800

    Remove all methods.

--- src/main/java/com/frankie/git_test/Test.java
+++ src/main/java/com/frankie/git_test/Test.java
@@ -9,16 +9,4 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
     }
-
-    public void a1(){
-        System.out.println("a1");
-    }
-
-    public void b1(){
-        System.out.println("b1");
-    }
-
-    public void b2(){
-        System.out.println("b2");
-    }
 }
